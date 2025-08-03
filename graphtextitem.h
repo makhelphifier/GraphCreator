490c0095ca42bb0991177e128998614959132f9f
makhel <makhelphifier@163.com>
2025-08-03 23:15:49 +0800

    完善文本框功能

--- graphtextitem.h
+++ graphtextitem.h
@@ -2,16 +2,30 @@
 #define GRAPHTEXTITEM_H
 
 #include <QGraphicsTextItem>
-
 class GraphTextItem : public QGraphicsTextItem
 {
     Q_OBJECT
 public:
     explicit GraphTextItem(const QString &text,QGraphicsItem* parent= nullptr);
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
+protected:
+    void mouseDoubleClickEvent(QGraphicsSceneMouseEvent *event) override;
+    void mousePressEvent(QGraphicsSceneMouseEvent *event) override;
+    void mouseReleaseEvent(QGraphicsSceneMouseEvent *event) override;
+    void focusOutEvent(QFocusEvent *event) override;
+
+
 private:
     // 辅助函数：绘制单个手柄
     void drawHandle(QPainter *painter, const QPointF &pos, qreal size);
+    enum HandlePosition{
+        None,TopLeft,TopMiddle,TopRight,
+        MiddleLeft,MiddleRight,
+        BottomLeft,BottomMiddle,BottomRight
+    };
+    HandlePosition m_activeHandle = None;
+    HandlePosition currentHandlePosition(const QPointF &currentPos);
+    QString handleToString(GraphTextItem::HandlePosition handle);
 };
 
 #endif // GRAPHTEXTITEM_H
